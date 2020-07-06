d00e7f1350b118e79cc991c9279687ec3944c4c0
varuntyagi275 <varuntyagi275@gmail.com>
2020-07-06 19:55:50 +0530

    Updated Task Assignment

---
+++ TaskApptunix/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlocksComponent } from './view/blocks/blocks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlocksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TaskApptunix/src/app/view/blocks/blocks.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blocks',
+  templateUrl: './blocks.component.html',
+  styleUrls: ['./blocks.component.css']
+})
+export class BlocksComponent implements OnInit {
+
+  items=[{
+    image:"",
+    number:1
+  },
+  {
+    image:"",
+    number:2
+  },
+  {
+    image:"",
+    number:3
+  },
+  {
+    image:"",
+    number:4
+  },
+  {
+    image:"",
+    number:5
+  },{
+    image:"",
+    number:6
+  }
+  ]
+displayList=[]
+
+  flag=false
+  constructor() { }
+  counter=0
+  reverse:number
+  ngOnInit(): void {
+  }
+images="assets/avataaars.png"
+  remove(){   //this method is to remove image
+   
+      this.displayList.pop();
+         }
+  
+  
+  update(e,j){ //this method is to update image
+            
+      for(let i=0;i<=this.displayList.length;i++){
+        if(i==j){
+          var reader= new FileReader();
+       reader.readAsDataURL(e.target.files[0]);
+      console.log(e.target.files[0])
+      reader.onload=(event:any)=>{
+      let temp=event.target.result      
+        this.displayList[i]=temp;
+        console.log("Updated")
+        }
+
+      }
+
+    }
+  }
+
+  addFile(e){  //this method is to add File
+    if(e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      console.log(e.target.files[0])
+      reader.onload=(event:any)=>{
+        let temp=event.target.result;
+        this.displayList.push(temp);
+      }
+    }
+  }
+}
